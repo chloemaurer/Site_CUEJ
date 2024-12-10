e0ddea6714933b1b9c943216b3b4367be42f4fb1
anjakely18 <anjaniaina03@gmail.com>
2024-12-11 00:42:15 +0100

    gestion menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-const menu = document.getElementById("menu-icon");
+console.log("Le JavaScript fonctionne !");
+
+const menuIcon = document.getElementById("menu-icon");
+const menuNav = document.getElementById("menu-nav");
+const xIcon = document.getElementById("x-icon");
+
+menuIcon.addEventListener("click", () => {
+  if (menuNav.classList.contains("d-none")) {
+    menuNav.classList.remove("d-none");
+    menuNav.classList.add("d-flex");
+  } else {
+    menuNav.classList.add("d-none");
+    menuNav.classList.remove("d-flex");
+  }
+});
+
+xIcon.addEventListener("click", () => {
+  if (menuNav.classList.contains("d-none")) {
+    menuNav.classList.remove("d-none");
+    menuNav.classList.add("d-flex");
+  } else {
+    menuNav.classList.add("d-none");
+    menuNav.classList.remove("d-flex");
+  }
+});

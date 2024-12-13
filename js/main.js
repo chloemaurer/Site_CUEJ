51195361008785002de68d0e8541daf5cce0a470
Chloe <86164066+chloemaurer@users.noreply.github.com>
2024-12-13 10:55:53 +0100

    js pb

--- js/main.js
+++ js/main.js
@@ -1,62 +1,47 @@
 "use strict";
 
-// Script pour ouvrir et fermer le menu
-// Script pour ouvrir et fermer le menu
-const nav = document.getElementById("nav");
-const menu = document.getElementById("menu");
-const closeMenu = document.getElementById("closeMenu");
-
-nav.addEventListener("click", () => {
-  menu.classList.add("active");
-});
-
-closeMenu.addEventListener("click", () => {
-  menu.classList.remove("active");
-});
-// ------------------------------------------------------------
-
 const options = {
-  defaultSpeed: "1.00",
-  speeds: ["1.25", "1.50", "2.00", "0.75"],
-  loop: true,
-  skipBackInterval: 15,
-  jumpForwardInterval: 15,
-  features: [
-    "playpause",
-    "progress",
-    "current",
-    "duration",
-    "skipback",
-    "changespeed",
-    "volume",
-    "jumpforward",
-  ],
-};
+    defaultSpeed: '1.00',
+    speeds: ['1.25', '1.50', '2.00', '0.75'],
+    loop: true,
+    skipBackInterval: 15,
+    jumpForwardInterval: 15,
+    features: [
+        "playpause",
+        "progress",
+        "current",
+        "duration",
+        "skipback",
+        "changespeed",
+        "volume",
+        "jumpforward",
+    ]
+}
 
-new MediaElementPlayer(document.querySelector("audio"), options);
+new MediaElementPlayer(
+    document.querySelector("audio"),
+    options
+);
 
 // Separate the audio controls so I can style them better.
 (() => {
-  const elementTop = document.createElement("div");
-  const elementBottom = document.createElement("div");
-  elementTop.classList.add("mejs-prepended-buttons");
-  elementBottom.classList.add("mejs-appended-buttons");
+    const elementTop = document.createElement('div');
+    const elementBottom = document.createElement('div');
+    elementTop.classList.add('mejs-prepended-buttons');
+    elementBottom.classList.add('mejs-appended-buttons');
 
-  const controls = document.querySelector(".mejs__controls");
-  controls.prepend(elementTop);
-  controls.append(elementBottom);
+    const controls = document.querySelector('.mejs__controls');
+    controls.prepend(elementTop);
+    controls.append(elementBottom);
 
-  const controlsChildren = Array.from(controls.childNodes).filter((v) =>
-    v.className.startsWith("mejs_")
-  );
+    const controlsChildren = Array.from(controls.childNodes).filter(v => v.className.startsWith("mejs_"));
 
-  controlsChildren.slice(0, 3).forEach((elem) => {
-    elementTop.append(elem);
-  });
+    controlsChildren.slice(0, 3).forEach(elem => {
+        elementTop.append(elem)
+    });
 
     controlsChildren.slice(3, controlsChildren.length).forEach(elem => {
         elementBottom.append(elem)
     })
 })()
-
 

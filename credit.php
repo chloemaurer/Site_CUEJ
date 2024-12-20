5edb940ad5cb2908d0bfc275a49b904743d7e4d8
anjakely18 <anjaniaina03@gmail.com>
2024-12-20 23:13:52 +0100

    modif

---
+++ credit.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crédits</title>
+    <link rel="stylesheet" href="./CSS/credit.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Anybody&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- En-tête -->
+    <header class="header">
+        <img src="./images/full.jpg" alt="l'équipe en entier">
+        <p class="credit">Crédits : Louis Le Bacquer</p>
+    </header>
+    <main>
+        <!-- Section Remerciements -->
+        <section class="credits-section">
+            <div class="container text-center">
+                <img src="./images/logo.svg" alt="logo du webdoc" class="img-fluid mb-3">
+                <h1 class="fw-bold display-4">crédits</h1>
+                <p class="lead">Remerciements et contributions</p>
+            </div>
+
+            <h2 class="fw-bold mb-4">Remerciements</h2>
+            <p>
+                Un grand merci aux personnes ayant donné de leur temps pour ce
+                web-documentaire, en particulier à Emma Conquet pour ses conseils avisés, à
+                celles et ceux qui apparaissent dans les sujets, aux encadrant·es et aux étudiantes
+                de l’IUT d’Haguenau.
+            </p>
+            <div class="gallery">
+                <img src="./images/1.jpg" alt="">
+                <img src="./images/2.jpg" alt="">
+                <img src="./images/3.jpg" alt="">
+                <img src="./images/4.jpg" alt="">
+                <img src="./images/5.jpg" alt="">
+                <img src="./images/6.jpg" alt="">
+        </section>
+
+        <!-- Section Équipe -->
+        <section class="credits-section">
+            <h3 class="fw-bold mb-4">directeur de publication</h3>
+            <p>Cédric Pellen</p>
+
+            <h3 class="fw-bold mb-4">étudiants du cuej</h3>
+            <ul>
+                <li>Rédaction en chef : Ismérie Vergne</li>
+                <li>Responsables iconographie : Liza Hervy-Marquer et Leyla Sobler</li>
+                <li>Responsables édition : Yanis Drouin et Marie Starecki</li>
+                <li>Rédacteurs et rédactrices : François Bertrand, Léa Bouquet, Garance Cailliet, Lucie Campoy, Louisa Chausse-Dumont, Yanis Drouin, Camille Fraioli, Arthur Guillamo, Liza Hervy-Marquer, Kenza Lalouni, Loïc Michels, Gustave Pinard, Aurore Ployer, Louise Pointin, Yves Poulain, Chantal Ried, Lya Roisin-Pillot, Almamy Sané, Jules Schnitzler, Leyla Sobler, Marie Starecki, Tristan Vanuxem, Ismérie Vergne, Élise Walle</li>
+            </ul>
+
+            <h3 class="fw-bold mb-4">étudiants en but métiers du multimédia et de l'internet</h3>
+            <ul>
+                <li>Team Création Numérique : Océane Cardot, Léa Dima, Amanda Marmillod, Sadaf Zebetian</li>
+                <li>Team Dévelopement Web : Chloé Maurer, Anja Rasamoelina</li>
+            </ul>
+
+            <h3 class="fw-bold mb-4">encadrement</h3>
+            <p>CUEJ : Antoine Bonin, Jean-Christophe Gallen, Antoine Giniaux, Joseph Pasquier, Mathias Thura</p>
+            <p>IUT de Haguenau : Sophie Béhal, Antonio Capobianco, David Cazier</p>
+        </section>
+        <h3 class="fw-bold mb-4">l'équipe en images</h3>
+        <!-- Section Photos supplémentaires -->
+        <section class="gallery">
+
+            <img src="./images/12.jpg" alt="">
+            <img src="./images/11.jpg" alt="">
+            <img src="./images/10.jpg" alt="">
+            <img src="./images/9.jpg" alt="">
+            <img src="./images/8.jpg" alt="">
+            <p>Crédits photos : Chantal Ried</p>
+
+        </section>
+
+        <!-- Section Logos -->
+        <section class="logos">
+            <h3>en partenariat avec</h3>
+            <div class="flex">
+
+                <img src="./images/cuej.png" alt="Cuej" class="img-fluid" style="max-height: 100px; width: auto;">
+
+
+                <img src="./images/iut.png" alt="IUT Haguenau" class="img-fluid" style="max-height: 100px; width: auto;">
+            </div>
+
+        </section>
+    </main>
+</body>
+
+</html>
